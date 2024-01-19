e44424c603a612463a26b37346057a0c12589f1c
WandiaG <wandiagithinji@gmail.com>
2024-01-19 05:59:30 -0800

    fixing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#Install flask / Version must be 2.1.0
+# puppet declarative script to install flask from pip3.
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
